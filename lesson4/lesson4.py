f472fe12124849c37cccae320ffa3bf93ae1769a
SamChen <sampitcher1976@gmail.com>
2025-11-16 10:47:26 +0800

    增加第四周上課內容

---
+++ lesson4/lesson4.py
@@ -0,0 +1,12 @@
+chinese = int(input("國文分數"))
+print(type(chinese))
+english = int(input("英文分數"))
+print(type(english))
+math = int(input("數學分數"))
+print(type(math))
+
+total = chinese + english + math
+print("總分 : ", total, "分", set = "__")
+average = round(total / 3, ndigits=2)
+print("平均 : ", average, "分", sep="---")
+
